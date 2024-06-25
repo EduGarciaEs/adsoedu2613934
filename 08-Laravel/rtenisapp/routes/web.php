928e7b3505762ff58e53261c249788f9bcd206be
davidandresmorales <dmoralesguapacha684@gmail.com>
2024-06-24 21:04:52 -0500

    Updating Blade

--- 08-Laravel/rtenisapp/routes/web.php
+++ 08-Laravel/rtenisapp/routes/web.php
@@ -1,64 +1,24 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/tenis/list',function(){
-    $tenis=App\Models\Tenis::all();
-    //echo var_dump($tenis);
-    dd($tenis->toArray());
+Route::get('catalogue', function () {
+    return view('catalogue');
 });
 
-Route::get('/tenis/{id}',function($id){
-    $tenis=App\Models\Tenis::find($id);
-    //echo var_dump($tenis);
-    dd($tenis->toArray());
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/tenis/show/{id}',function($id){
-    $id=request(id);
-    $tenis=App\Models\Tenis::find($id);
-    //echo var_dump($tenis);
-    dd($tenis->toArray());
-});
-
-Route::get('/tenis',function(){
-    $tenis=App\Models\Tenis::all();
-    //echo var_dump($tenis);
-    return view('listtenis')->with('tenis',$tenis);
-});
-
-
-
-Route::get('/tenis/user/create',function(){
-    $user=\App\Models\User::get()->take(20);
-    echo "<table><tr>
-                 <th>ID</th>
-                 <th>Fullname</th>
-                 <th>Age</th>
-                 <th>Birthdate</th>
-            </tr></br>";
-    foreach ($user as $use) {
-        $carbonated_date =Carbon::parse($use[birthdate])->age;
-        echo "
-        <tr>
-                 <td>" . $use->id . "</td>
-                 <td>" . $use->fullname . "</td>
-                 <td>" . $carbonated_date . " years old</td>
-                 <td>" . $use->created_at->diffForHumans() . "</td>
-                 
-      
-            </tr></br>
-            </table>
-            ";
-
-    }
-});
-
-
-
-
-
+require __DIR__.'/auth.php';
